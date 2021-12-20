bf4da44e8180abce60ee4371323c7b412aa272f6
frknayk <furkanayik@outlook.com>
2021-12-20 21:09:15 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PhotoMath
 Perform math operations from image
+
+## 1. Create the conda environment
+- ```conda create -n photomath python=3.7```
+- ```conda activate photomath```
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='PhotoMath',
+    version='0.1',
+    description='Perform mathematical expressions from images directly',
+    author='Furkan Ayik',
+    author_email='furkanayik@outlook.com'
+)
+
+
